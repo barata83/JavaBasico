d02a3748ef695a9b0a56533ad5385c28e1740c5a
leandro <marcelleandro@outlook.com>
2025-05-17 00:34:53 -0300

    1752025

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-import gui.SudokuGUI;
-import javax.swing.SwingUtilities;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new SudokuGUI().setVisible(true));
-    }
-}
